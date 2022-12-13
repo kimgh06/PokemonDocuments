f40ff92eb4733de58b196d98204eec972f1f0e99
kkh061101 <kkh061101@naver.com>
2022-12-13 09:03:25 +0900

    UI 일부 수정

--- src/Components/Bookmark.js
+++ src/Components/Bookmark.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Bookmark({ Bookmark }) {
   return <div className="BookMark">
-    <span style={{ marginLeft: '2em' }}><b>Bookmark</b></span>
+    <span><b>Bookmark</b></span>
     <div className="BookMarkList">
       {Bookmark.map((i, n) => {
         return <div key={n}><b className="pokeNum" style={{ color: `${i.color}` }}>{i.id}</b>&nbsp;{i.name}</div>
